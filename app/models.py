b87b02cbbde5b60333bfdbe13164679042c4ec61
bilalovrevan <bilalov_revan@bk.ru>
2025-03-06 22:30:15 +0100

    fixed

--- app/models.py
+++ app/models.py
@@ -10,7 +10,12 @@ with open(model_path, "rb") as file:
     loaded_model, loaded_data = pickle.load(file)
 
 def predict_risk(features: list[int | float | str]) -> int:
-    """Predict the risk based on the input parameters."""
+    """Predict the risk based on the input parameters.
+    Args:
+        features (list[int|float|str]): The input features for the model
+    Returns:
+        int: The predicted risk value
+    """
     loaded_data = pd.DataFrame([features], columns=[
         "Age", "Sex", "ChestPainType", "Cholesterol", "FastingBS",
         "MaxHR", "ExerciseAngina", "Oldpeak", "ST_Slope"
@@ -18,6 +23,10 @@ def predict_risk(features: list[int | float | str]) -> int:
 
     # Dummy prediction logic (replace this with your actual model prediction)
     # prediction = loaded_model.predict(loaded_data)
+    print(features)
     prediction = 1 if loaded_data["Age"].values[0] > 50 else 0
-
-    return prediction
+    # Make prediction
+    # prediction: list[int] = loaded_model.predict([features])
+    # Return the prediction result
+    # return prediction[0]
+    return 0
