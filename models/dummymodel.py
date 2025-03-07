4d4f8708c43db31c55222b61fd93a4ea81df0a6e
Reeju Bhattacherji <reejubhattacherji@gmail.com>
2025-03-07 13:34:45 +0100

    gautam update

--- models/dummymodel.py
+++
@@ -1,9 +0,0 @@
-import pickle
-import os
-
-dummy_model = "This is a dummy model"
-dummy_data = {"example": 123}
-
-os.makedirs("models", exist_ok=True)
-with open("models/model_and_data.pkl", "wb") as file:
-    pickle.dump((dummy_model, dummy_data), file)
